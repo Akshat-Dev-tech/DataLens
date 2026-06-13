b53e1f61a07b00e005849b43c4d1419f2e52f14b
Akshat-Dev-tech <akiiwork1234@gmail.com>
2026-06-13 12:29:36 +0530

    feat:Integrated Posts API and display data

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+
+//Display Posts
+const DisplayPosts = ({posts}) =>{
+  return (
+    <>
+    <h4>Here are your posts</h4>
+    {
+      posts && posts?.length>0 && posts.map((post)=>
+          <div className='display-post '>
+            <h5>{post.title}</h5>
+            <p>{post.body}</p>
+          </div>
+        )
+    }
+    </>
+  )
+}
+
+function App() {
+  const [posts , setPosts]= useState([])
+
+  //Fetch Posts
+  const getPosts = async() =>{
+    const fetchposts = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await fetchposts.json()
+    setPosts(data)
+    console.log('Posts', data)
+  }
+
+  useEffect(()=>{
+    getPosts()
+  },[])
+
+
+  return (
+    <>
+      <h1>Data Lens</h1>
+      <DisplayPosts posts={posts}/>
+    </>
+  )
+}
+
+export default App
